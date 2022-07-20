14fc2eeb97ed4123b8261c261df2ff9e9e622470
miladev-ent <milwad.dev@gmail.com>
2022-07-20 22:02:32 +0430

    move user file to user module

---
+++ Modules/User/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\User\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $casts = ['email_verified_at' => 'datetime'];
+}
--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => \Modules\User\Models\User::class,
         ],
 
         // 'users' => [
