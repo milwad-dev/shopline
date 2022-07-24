74342a779bc8f85489f1a481f09cdb9d1ebc76b2
miladev-ent <milwad.dev@gmail.com>
2022-07-24 22:55:20 +0430

    make custom file for login with social  & change auth pages

--- Modules/Auth/Resources/Views/register.blade.php
+++ Modules/Auth/Resources/Views/register.blade.php
@@ -90,22 +90,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6 pr-30 d-none d-lg-block">
-                                <div class="card-login mt-115">
-                                    <a href="page-register.html#" class="social-login facebook-login">
-                                        <img src="{{ asset('home/imgs/theme/icons/logo-facebook.svg') }}" alt="">
-                                        <span>Continue with Facebook</span>
-                                    </a>
-                                    <a href="page-register.html#" class="social-login google-login">
-                                        <img src="{{ asset('home/imgs/theme/icons/logo-google.svg') }}" alt="">
-                                        <span>Continue with Google</span>
-                                    </a>
-                                    <a href="page-register.html#" class="social-login apple-login">
-                                        <img src="{{ asset('home/imgs/theme/icons/logo-apple.svg') }}" alt="">
-                                        <span>Continue with Apple</span>
-                                    </a>
-                                </div>
-                            </div>
+                            @include('Auth::section.login-social')
                         </div>
                     </div>
                 </div>
---
+++ Modules/Auth/Resources/Views/section/login-social.blade.php
@@ -0,0 +1,16 @@
+<div class="col-lg-6 pr-30 d-none d-lg-block">{{-- TODO --}}
+    <div class="card-login mt-115">
+        <a href="#" class="social-login facebook-login">
+            <img src="{{ asset('home/imgs/theme/icons/logo-facebook.svg') }}" alt="">
+            <span>Continue with Facebook</span>
+        </a>
+        <a href="#" class="social-login google-login">
+            <img src="{{ asset('home/imgs/theme/icons/logo-google.svg') }}" alt="">
+            <span>Continue with Google</span>
+        </a>
+        <a href="#" class="social-login apple-login">
+            <img src="{{ asset('home/imgs/theme/icons/logo-apple.svg') }}" alt="">
+            <span>Continue with Apple</span>
+        </a>
+    </div>
+</div>
