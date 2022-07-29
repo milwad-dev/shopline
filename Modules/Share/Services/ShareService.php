e84a60b422972ba88870cec2cb2bd631ab16b81d
miladev-ent <milwad.dev@gmail.com>
2022-07-29 23:02:46 +0430

    add phpdoc to share service file

--- Modules/Share/Services/ShareService.php
+++ Modules/Share/Services/ShareService.php
@@ -4,17 +4,35 @@ namespace Modules\Share\Services;
 
 class ShareService
 {
+    /**
+     * Show success toast.
+     *
+     * @param $title
+     * @return mixed
+     */
     public static function successToast($title)
     {
         return toast($title,'success')->autoClose(5000);
     }
 
+    /**
+     * Show error toast.
+     *
+     * @param $title
+     * @return mixed
+     */
     public static function errorToast($title)
     {
         return toast($title,'error')->autoClose(5000);
     }
 
-    public static function makeSlug($title)
+    /**
+     * Convert string to slug.
+     *
+     * @param string $title
+     * @return string
+     */
+    public static function makeSlug(string $title)
     {
         return preg_replace('/\s+/', '-', str_replace('_', '', $title));
     }
