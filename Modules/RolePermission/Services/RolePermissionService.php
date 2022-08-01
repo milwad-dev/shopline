09a2e9b2f76e1683edd36a43bf9a042cc7c27130
miladev-ent <milwad.dev@gmail.com>
2022-08-01 18:43:51 +0430

    add queue for store & update roles

--- Modules/RolePermission/Services/RolePermissionService.php
+++ Modules/RolePermission/Services/RolePermissionService.php
@@ -2,27 +2,48 @@
 
 namespace Modules\RolePermission\Services;
 
-use Modules\RolePermission\Models\RolePermission;
+use Modules\RolePermission\Repositories\RolePermissionRepo;
+use Spatie\Permission\Models\Role;
 
 class RolePermissionService
 {
+    /**
+     * Store role with assign permissions.
+     *
+     * @param  $request
+     * @return mixed
+     */
     public function store($request)
     {
-        return $this->query()->create([
-
-        ]);
+        return $this->query()
+            ->create(['name' => $request->name])
+            ->syncPermissions($request->permissions);
     }
 
+    /**
+     * Update role with sync permissions.
+     *
+     * @param  $request
+     * @param  $id
+     * @return mixed
+     */
     public function update($request, $id)
     {
-        return $this->query()->whereId($id)->update([
+        $roleRepo = new RolePermissionRepo;
+        $role = $roleRepo->findById($id);
 
-        ]);
+        return $role
+            ->syncPermissions($request->permissions)
+            ->update(['name' => $request->name]);
     }
 
+    /**
+     * Get query for model (builder).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     private function query()
     {
-        return RolePermission::query();
+        return Role::query();
     }
 }
-        
