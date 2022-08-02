3c7c85e02777deed7210236039add89d1a814dbb
miladev-ent <milwad.dev@gmail.com>
2022-08-03 00:36:46 +0430

    write phpdoc for role permission repo

--- Modules/RolePermission/Repositories/RolePermissionRepo.php
+++ Modules/RolePermission/Repositories/RolePermissionRepo.php
@@ -7,26 +7,53 @@ use Spatie\Permission\Models\Role;
 
 class RolePermissionRepo
 {
+    /**
+     * Get the latest roles with permissions.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function index()
     {
-        return $this->query()->latest();
+        return $this->query()->with('permissions')->latest();
     }
 
+    /**
+     * Find role by id.
+     *
+     * @param  $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     */
     public function findById($id)
     {
         return $this->query()->findOrFail($id);
     }
 
+    /**
+     * Delete role by id.
+     *
+     * @param $id
+     * @return mixed
+     */
     public function delete($id)
     {
         return $this->query()->where('id', $id)->delete();
     }
 
+    /**
+     * Get all permissions.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function getAllPermissions()
     {
         return Permission::all();
     }
 
+    /**
+     * Builder for queue model.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     private function query()
     {
         return Role::query();
