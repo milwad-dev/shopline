c2934abf887d6eb5f242c04f87e2eccc9286f749
miladev-ent <milwad.dev@gmail.com>
2022-08-03 01:32:16 +0430

    Update index.blade.php

--- Modules/RolePermission/Resources/Views/index.blade.php
+++ Modules/RolePermission/Resources/Views/index.blade.php
@@ -50,7 +50,7 @@
                                                                 <span>Edit</span>
                                                             </a>
                                                             <a class="dropdown-item"
-                                                               onclick="deleteItem(event, '{{ route('role-permissions.destroy', $role->id) }}')">
+                                                                onclick="deleteItem(event, '{{ route('role-permissions.destroy', $role->id) }}')">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash me-50"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
                                                                 <span>Delete</span>
                                                             </a>
