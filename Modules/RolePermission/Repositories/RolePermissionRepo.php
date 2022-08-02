71946e03b06a3091b6dc6cdf836a4ad356fcf6d3
miladev-ent <milwad.dev@gmail.com>
2022-08-03 00:29:57 +0430

    add queue to role permission repo

--- Modules/RolePermission/Repositories/RolePermissionRepo.php
+++ Modules/RolePermission/Repositories/RolePermissionRepo.php
@@ -2,24 +2,29 @@
 
 namespace Modules\RolePermission\Repositories;
 
-use Modules\RolePermission\Models\RolePermission;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolePermissionRepo
 {
     public function index()
     {
-
+        return $this->query()->latest();
     }
 
     public function findById($id)
     {
-
+        return $this->query()->findOrFail($id);
     }
 
     public function delete($id)
     {
         return $this->query()->where('id', $id)->delete();
+    }
+
+    public function getAllPermissions()
+    {
+        return Permission::all();
     }
 
     private function query()
