cbfe90687045143a31b8fcb568782a678c010dd2
miladev-ent <milwad.dev@gmail.com>
2022-08-03 00:39:24 +0430

    add phpdoc for permission model

--- Modules/RolePermission/Models/Permission.php
+++ Modules/RolePermission/Models/Permission.php
@@ -4,11 +4,19 @@ namespace Modules\RolePermission\Models;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
+    /**
+     * Add Permissions
+     */
     public const PERMISSION_SUPER_ADMIN = 'permission super admin';
     public const PERMISSION_PANEL = 'permission panel';
     public const PERMISSION_USERS = 'permission users';
     public const PERMISSION_CATEGORIES = 'permission categories';
 
+    /**
+     * Get permissions in array with static.
+     *
+     * @var array|string[]
+     */
     public static array $permissions = [
         self::PERMISSION_SUPER_ADMIN,
         self::PERMISSION_PANEL,
