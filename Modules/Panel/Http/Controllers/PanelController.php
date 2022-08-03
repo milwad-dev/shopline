33a98fc52261e3117d0746d6a001702b52e183c1
miladev-ent <milwad.dev@gmail.com>
2022-08-04 00:23:50 +0430

    add authorize for policy to panel module

--- Modules/Panel/Http/Controllers/PanelController.php
+++ Modules/Panel/Http/Controllers/PanelController.php
@@ -2,12 +2,14 @@
 
 namespace Modules\Panel\Http\Controllers;
 
+use Modules\Panel\Models\Panel;
 use Modules\Share\Http\Controllers\Controller;
 
 class PanelController extends Controller
 {
     public function __invoke()
     {
+        $this->authorize('manage', Panel::class);
         return view('Panel::index');
     }
 }
