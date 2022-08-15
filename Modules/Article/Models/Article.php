06966d862a09a21ed54d6aa4dd56e4f335876353
miladev-ent <milwad.dev@gmail.com>
2022-08-15 23:15:46 +0430

    add trait to article model

--- Modules/Article/Models/Article.php
+++ Modules/Article/Models/Article.php
@@ -4,8 +4,9 @@ namespace Modules\Article\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTags;
 }
