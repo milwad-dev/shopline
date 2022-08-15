49f7c5a37c6362f477f649322639f0aded5a3cff
miladev-ent <milwad.dev@gmail.com>
2022-08-15 22:45:55 +0430

    add article test

---
+++ Modules/Article/Tests/Feature/ArticleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Article\Tests\Feature;
+
+use Tests\TestCase;
+
+class ArticleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
