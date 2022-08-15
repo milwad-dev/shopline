94a28510132f1d4b47c3926aae669f44aa7754c2
miladev-ent <milwad.dev@gmail.com>
2022-08-15 22:06:38 +0430

    add xdebug for tests

--- Modules/Home/Repositories/HomeRepo.php
+++ Modules/Home/Repositories/HomeRepo.php
@@ -2,31 +2,7 @@
 
 namespace Modules\Home\Repositories;
 
-use Modules\Home\Models\Home;
-use Modules\Share\Contracts\Interface\RepositoriesInterface;
-use Modules\Share\Repositories\ShareRepo;
-
-class HomeRepo implements RepositoriesInterface
+class HomeRepo
 {
-    private string $class = Home::class;
 
-    public function index()
-    {
-
-    }
-
-    public function findById($id)
-    {
-
-    }
-
-    public function delete($id)
-    {
-
-    }
-
-    private function query()
-    {
-        return ShareRepo::query($this->class);
-    }
 }
