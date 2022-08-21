8b4c2db2aa609e78eb39f425c7f0d2b81b5946fe
miladev-ent <milwad.dev@gmail.com>
2022-08-21 20:46:34 +0430

    add phpdoc to panel route

--- Modules/Panel/Routes/panel_routes.php
+++ Modules/Panel/Routes/panel_routes.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Panel routes
+|--------------------------------------------------------------------------
+|
+| Here you can see panel routes.
+|
+*/
+
 Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
     $router->get('index', ['uses' => 'PanelController', 'as' => 'panel.index']);
 });
