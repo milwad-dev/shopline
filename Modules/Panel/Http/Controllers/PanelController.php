a0b2b83c87843147c38569c9aa1c8a6c3b10b373
miladev-ent <milwad.dev@gmail.com>
2022-08-21 20:36:03 +0430

    add phpdoc to panel controller

--- Modules/Panel/Http/Controllers/PanelController.php
+++ Modules/Panel/Http/Controllers/PanelController.php
@@ -7,6 +7,12 @@ use Modules\Share\Http\Controllers\Controller;
 
 class PanelController extends Controller
 {
+    /**
+     * Show the panel page & get some data.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function __invoke()
     {
         $this->authorize('manage', Panel::class);
