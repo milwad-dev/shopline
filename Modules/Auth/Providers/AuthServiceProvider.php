868e589fed7c4c5747153e9008db2cf5449b43cd
miladev-ent <milwad.dev@gmail.com>
2022-08-21 23:18:13 +0430

    refactor auth service provider

--- Modules/Auth/Providers/AuthServiceProvider.php
+++ Modules/Auth/Providers/AuthServiceProvider.php
@@ -7,13 +7,71 @@ use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * Get namespace for auth controllers.
+     *
+     * @var string
+     */
     public string $namespace = 'Modules\Auth\Http\Controllers';
 
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
+    private string $name = 'Auth';
+
+    /**
+     * Get middleware route.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web'];
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/auth_routes.php';
+
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Auth');
-        Route::middleware(['web'])
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
+    }
+
+    /**
+     * Load view files.
+     *
+     * @return void
+     */
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
+    }
+
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
+    private function loadRouteFiles(): void
+    {
+        Route::middleware($this->middlewareRoute)
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../Routes/auth_routes.php');
+            ->group(__DIR__ . $this->routePath);
     }
 }
