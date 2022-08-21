e4f930c2d26663dba3490d9d207756a09d713e62
miladev-ent <milwad.dev@gmail.com>
2022-08-21 21:00:21 +0430

    refactor home service provider

--- Modules/Home/Providers/HomeServiceProvider.php
+++ Modules/Home/Providers/HomeServiceProvider.php
@@ -7,23 +7,95 @@ use Illuminate\Support\ServiceProvider;
 
 class HomeServiceProvider extends ServiceProvider
 {
-    public string $namespace = 'Modules\Home\Http\Controllers';
+    /**
+     * Get namespace for home controllers.
+     *
+     * @var string
+     */
+    private string $namespace = 'Modules\Home\Http\Controllers';
 
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
+    private string $name = 'Home';
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/home_routes.php';
+
+    /**
+     * Get middleware route.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web', 'verify'];
+
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Home');
-
-        Route::middleware(['web', 'verify'])
-            ->namespace($this->namespace)
-            ->group(__DIR__ . '/../Routes/home_routes.php');
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
     }
 
+    /**
+     * Boot service provider.
+     *
+     * @return void
+     */
     public function boot()
     {
-        config()->set('panelConfig.menus.home', [ // Set menu for panel
+        $this->setMenuForPanel();
+    }
+
+    /**
+     * Load panel view files.
+     *
+     * @return void
+     */
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
+    }
+
+    /**
+     * Set menu for panel
+     *
+     * @return void
+     */
+    private function setMenuForPanel(): void
+    {
+        config()->set('panelConfig.menus.home', [
             'title' => 'Home',
-            'icon'  => 'home',
-            'url'   => route('home.index'),
+            'icon' => 'home',
+            'url' => route('home.index'),
         ]);
+    }
+
+    /**
+     * Load panel route files.
+     *
+     * @return void
+     */
+    private function loadRouteFiles(): void
+    {
+        Route::middleware($this->middlewareRoute)
+            ->namespace($this->namespace)
+            ->group(__DIR__ . $this->routePath);
     }
 }
