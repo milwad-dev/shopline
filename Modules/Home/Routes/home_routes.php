fa7fb6d923b6c0b5e8194b0419dc9d63fd2d74bc
miladev-ent <milwad.dev@gmail.com>
2022-08-21 20:48:24 +0430

    add phpdoc to home routes

--- Modules/Home/Routes/home_routes.php
+++ Modules/Home/Routes/home_routes.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Home routes
+|--------------------------------------------------------------------------
+|
+| Here you can see home routes.
+|
+*/
+
 Route::group([], static function ($router) {
     $router->get('/', ['uses' => 'HomeController@index', 'as' => 'home.index']);
 });
