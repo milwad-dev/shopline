3c3176ee06e281bc41dd629b461f6d228bac2979
miladev-ent <milwad.dev@gmail.com>
2022-08-21 19:39:05 +0430

    add phpdoc to ajax resposne

--- Modules/Share/Responses/AjaxResponses.php
+++ Modules/Share/Responses/AjaxResponses.php
@@ -2,17 +2,30 @@
 
 namespace Modules\Share\Responses;
 
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class AjaxResponses
 {
-    public static function SuccessResponse()
+    /**
+     * Return json response with status 200 (success).
+     *
+     * @param  string $message
+     * @return JsonResponse
+     */
+    public static function SuccessResponse(string $message = 'The operation was successful.')
     {
-        return response()->json(['message' => 'The operation was successful.']);
+        return response()->json(['message' => $message]);
     }
 
-    public static function FailedResponse()
+    /**
+     * Return json response with status 500 (serve error).
+     *
+     * @param  string $message
+     * @return JsonResponse
+     */
+    public static function FailedResponse(string $message = 'The operation was not successful.')
     {
-        return response()->json(['message' => 'The operation was not successful.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        return response()->json(['message' => $message], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
