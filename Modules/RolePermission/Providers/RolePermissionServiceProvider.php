abb102ffd45f00620ba50531e343e8d96a133179
miladev-ent <milwad.dev@gmail.com>
2022-08-21 19:52:39 +0430

    refactor role permission service provider

--- Modules/RolePermission/Providers/RolePermissionServiceProvider.php
+++ Modules/RolePermission/Providers/RolePermissionServiceProvider.php
@@ -14,11 +14,39 @@ use Modules\RolePermission\Policies\RolePermissionPolicy;
 class RolePermissionServiceProvider extends ServiceProvider
 {
     /**
-     * Set namespace for controllers.
+     * Get namespace for controllers.
      *
      * @var string
      */
-    public string $namespace = 'Modules\RolePermission\Http\Controllers';
+    private string $namespace = 'Modules\RolePermission\Http\Controllers';
+
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
+    private string $name = 'RolePermission';
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/rolepermission_routes.php';
+
+    /**
+     * Get middleware route.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web', 'verify'];
 
     /**
      * Register files.
@@ -27,30 +55,111 @@ class RolePermissionServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(PermissionSeeder::class, PermissionTableSeeder::class);
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'RolePermission');
+        $this->loadMigrationFiles();
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
+        $this->loadPolicyFiles();
 
-        DatabaseSeeder::$seeders[] = PermissionSeeder::class;
-        Route::middleware(['web', 'verify'])->namespace($this->namespace)
-        ->group(__DIR__ . '/../Routes/rolepermission_routes.php');
-        Gate::policy(Permission::class, RolePermissionPolicy::class);
-        Gate::before(static function ($user) {
-            return $user->hasPermissionTo(Permission::PERMISSION_SUPER_ADMIN) ? true : null;
-        });
+        $this->bindSeeder();
+        $this->setDatabaseSeederWithPermissionSeeder();
+        $this->setGateBefore();
     }
 
     /**
-     * Set menu.
+     * Set menu for panel.
      *
      * @return void
      */
     public function boot()
     {
-        config()->set('panelConfig.menus.role-permissions', [ // Set menu for panel
+        $this->setMenuForPanel();
+    }
+
+    /**
+     * Set menu for panel.
+     *
+     * @return void
+     */
+    private function setMenuForPanel(): void
+    {
+        config()->set('panelConfig.menus.role-permissions', [
             'title' => 'Role & Permissions',
-            'icon'  => 'alert-triangle',
-            'url'   => route('role-permissions.index'),
+            'icon' => 'alert-triangle',
+            'url' => route('role-permissions.index'),
         ]);
+    }
+
+    /**
+     * Bind permission seeder.
+     *
+     * @return void
+     */
+    private function bindSeeder(): void
+    {
+        $this->app->bind(PermissionSeeder::class, PermissionTableSeeder::class);
+    }
+
+    /**
+     * Load permission migration files.
+     *
+     * @return void
+     */
+    private function loadMigrationFiles(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    /**
+     * Load permission view files.
+     *
+     * @return void
+     */
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
+    }
+
+    /**
+     * Set database seeder with permission seeder.
+     *
+     * @return void
+     */
+    private function setDatabaseSeederWithPermissionSeeder(): void
+    {
+        DatabaseSeeder::$seeders[] = PermissionSeeder::class;
+    }
+
+    /**
+     * Load permission route files.
+     *
+     * @return void
+     */
+    private function loadRouteFiles(): void
+    {
+        Route::middleware($this->middlewareRoute)
+            ->namespace($this->namespace)
+            ->group(__DIR__ . $this->routePath);
+    }
+
+    /**
+     * Load permission policy files.
+     *
+     * @return void
+     */
+    private function loadPolicyFiles(): void
+    {
+        Gate::policy(Permission::class, RolePermissionPolicy::class);
+    }
+
+    /**
+     * Set gate before for super admin permission.
+     *
+     * @return void
+     */
+    private function setGateBefore(): void
+    {
+        Gate::before(static function ($user) {
+            return $user->hasPermissionTo(Permission::PERMISSION_SUPER_ADMIN) ? true : null;
+        });
     }
 }
