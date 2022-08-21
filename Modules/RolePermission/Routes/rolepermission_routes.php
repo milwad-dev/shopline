5f697f70fefe24663319f0495dae93b9dcdb44e1
miladev-ent <milwad.dev@gmail.com>
2022-08-21 19:57:38 +0430

    add phpdoc to role permission routes

--- Modules/RolePermission/Routes/rolepermission_routes.php
+++ Modules/RolePermission/Routes/rolepermission_routes.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Role permission routes
+|--------------------------------------------------------------------------
+|
+| Here you can see user role permission routes.
+|
+*/
+
 Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
     $router->resource('role-permissions', 'RolePermissionController', ['except' => 'show']); // Remove show
 });
