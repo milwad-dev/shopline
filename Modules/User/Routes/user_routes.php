36b6a7ce8a15227d89f7b7e01ea6b711606a18d0
miladev-ent <milwad.dev@gmail.com>
2022-08-21 15:37:05 +0430

    add phpdoc to user routes

--- Modules/User/Routes/user_routes.php
+++ Modules/User/Routes/user_routes.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| User routes
+|--------------------------------------------------------------------------
+|
+| Here you can see user routes.
+|
+*/
+
 Route::group(['middleware' => 'auth', 'prefix' => 'panel'], static function ($router) {
     $router->resource('users', 'Panel\UserController');
 });
