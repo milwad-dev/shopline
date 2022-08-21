cb912e5d48eacd54e56eec24bff2178d3069f23f
miladev-ent <milwad.dev@gmail.com>
2022-08-21 21:03:18 +0430

    move category controller

--- Modules/Category/Routes/category_routes.php
+++ Modules/Category/Routes/category_routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'panel', 'middleware' => 'auth', 'namespace' => 'Panel'], static function ($router) {
+Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
     $router->patch('categories/{id}/active', ['uses' => 'CategoryController@active', 'as' => 'categories.change.status.active']);
     $router->patch('categories/{id}/inactive', ['uses' => 'CategoryController@inactive', 'as' => 'categories.change.status.inactive']);
     $router->resource('categories', 'CategoryController');
