20529dd1988ed81660da66146deaa1dbdbe36812
miladev-ent <milwad.dev@gmail.com>
2022-08-23 01:46:01 +0430

    refactor slider service provider

--- Modules/Slider/Providers/SliderServiceProvider.php
+++ Modules/Slider/Providers/SliderServiceProvider.php
@@ -7,14 +7,89 @@ use Illuminate\Support\Facades\Route;
 
 class SliderServiceProvider extends ServiceProvider
 {
-    public string $namespace = 'Modules\Slider\Http\Controllers';
+    /**
+     * Get namespace for slider controllers.
+     *
+     * @var string
+     */
+    private string $namespace = 'Modules\Slider\Http\Controllers';
 
+    /**
+     * Get migration path.
+     *
+     * @var string
+     */
+    private string $migrationPath = '/../Database/Migrations';
+
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
+    private string $name = 'Slider';
+
+    /**
+     * Get middleware route.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web', 'verify'];
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/slider_routes.php';
+
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Slider');
+        $this->loadMigrationFiles();
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
+    }
 
-        Route::middleware(['web', 'verify'])->namespace($this->namespace)
-        ->group(__DIR__ . '/../Routes/slider_routes.php');
+    /**
+     * Load migration files.
+     *
+     * @return void
+     */
+    private function loadMigrationFiles(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
+    }
+
+    /**
+     * Load view files.
+     *
+     * @return void
+     */
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
+    }
+
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
+    private function loadRouteFiles(): void
+    {
+        Route::middleware($this->middlewareRoute)
+            ->namespace($this->namespace)
+            ->group(__DIR__ . $this->routePath);
     }
 }
