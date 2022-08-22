87e42c32e367cc13bf50ce8dfe9beec53e9a85c1
miladev-ent <milwad.dev@gmail.com>
2022-08-23 01:11:59 +0430

    refactor article service provider

--- Modules/Article/Providers/ArticleServiceProvider.php
+++ Modules/Article/Providers/ArticleServiceProvider.php
@@ -10,21 +10,121 @@ use Modules\Article\Policies\ArticlePolicy;
 
 class ArticleServiceProvider extends ServiceProvider
 {
-    public string $namespace = 'Modules\Article\Http\Controllers';
+    /**
+     * Get namespace for article controllers.
+     *
+     * @var string
+     */
+    private string $namespace = 'Modules\Article\Http\Controllers';
 
+    /**
+     * Get migration path.
+     *
+     * @var string
+     */
+    private string $migrationPath = '/../Database/Migrations';
+
+    /**
+     * Get middleware route.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web', 'verify'];
+
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
+    private string $name = 'Article';
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/article_routes.php';
+
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Article');
+        $this->loadMigrationFiles();
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
+        $this->loadPolicyFiles();
+    }
 
-        Route::middleware(['web', 'verify'])->namespace($this->namespace)
-        ->group(__DIR__ . '/../Routes/article_routes.php');
+    /**
+     * Boot service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setMenuForPanel();
+    }
+
+    /**
+     * Load panel policy files.
+     *
+     * @return void
+     */
+    private function loadPolicyFiles(): void
+    {
         Gate::policy(Article::class, ArticlePolicy::class);
     }
 
-    public function boot()
+    /**
+     * Load panel route files.
+     *
+     * @return void
+     */
+    private function loadRouteFiles(): void
     {
-        config()->set('panelConfig.menus.articles', [ // Set menu for panel
+        Route::middleware($this->middlewareRoute)
+            ->namespace($this->namespace)
+            ->group(__DIR__ . $this->routePath);
+    }
+
+    /**
+     * Load panel view files.
+     *
+     * @return void
+     */
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
+    }
+
+    /**
+     * Load panel migration files.
+     *
+     * @return void
+     */
+    private function loadMigrationFiles(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
+    }
+
+    /**
+     * Set menu for panel
+     * 
+     * @return void
+     */
+    private function setMenuForPanel(): void
+    {
+        config()->set('panelConfig.menus.articles', [
             'title' => 'Article',
             'icon' => 'book',
             'url' => route('articles.index'),
