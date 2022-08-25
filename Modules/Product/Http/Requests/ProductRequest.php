cefb4f2aa78deadb5ede84e46573d4bb8ea3fb70
miladev-ent <milwad.dev@gmail.com>
2022-08-25 12:05:38 +0430

    add product request

--- Modules/Product/Http/Requests/ProductRequest.php
+++ Modules/Product/Http/Requests/ProductRequest.php
@@ -3,6 +3,8 @@
 namespace Modules\Product\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use Modules\Product\Enums\ProductStatusEnum;
 
 class ProductRequest extends FormRequest
 {
@@ -23,8 +25,25 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
+        $rules = [
+            'first_media_id'  => 'required|file|mimes:jpeg,png,jpg|max:2048',
+            'second_media_id' => 'required|file|mimes:jpeg,png,jpg|max:2048',
+            'title' => 'required|string|min:3|max:255|unique:products,title',
+            'price' => 'required|numeric',
+            'count' => 'required|numeric',
+            'type' => 'required|string|max:255',
+            'short_description' => 'required|string|min:3',
+            'body' => 'required|string|min:3',
+            'status' => ['required', 'string', 'max:255', new Enum(ProductStatusEnum::class)],
+            'categories' => 'required|array',
         ];
+
+        if (request()->method === 'PATCH') {
+            $rules['first_media_id'] = 'nullable|file|mimes:jpeg,png,jpg|max:2048';
+            $rules['second_media_id'] = 'nullable|file|mimes:jpeg,png,jpg|max:2048';
+            $rules['title'] = 'required|string|min:3|max:255|unique:products,title,' . request()->id;
+        }
+
+        return $rules;
     }
 }
