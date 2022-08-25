8df40c813e3aeee33385b1fd5ef4e8346c996581
miladev-ent <milwad.dev@gmail.com>
2022-08-25 12:00:00 +0430

    add phpdoc to product repository

--- Modules/Product/Repositories/ProductRepoEloquent.php
+++ Modules/Product/Repositories/ProductRepoEloquent.php
@@ -6,21 +6,43 @@ use Modules\Product\Models\Product;
 
 class ProductRepoEloquent implements ProductRepoEloquentInterface
 {
+    /**
+     * Get latest products.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function index()
     {
         return $this->query()->latest();
     }
 
+    /**
+     * Find product by id.
+     *
+     * @param  $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     */
     public function findById($id)
     {
         return $this->query()->findOrFail($id);
     }
 
+    /**
+     * Delete product by id.
+     *
+     * @param  $id
+     * @return mixed
+     */
     public function delete($id)
     {
         return $this->query()->where('id', $id)->delete();
     }
 
+    /**
+     * Get query model (builder).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     private function query()
     {
         return Product::query();
