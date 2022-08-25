4a46d1cd2bb388716dbbd24ee4da5611cf541a85
miladev-ent <milwad.dev@gmail.com>
2022-08-25 11:50:33 +0430

    add comment to product route

--- Modules/Product/Routes/product_routes.php
+++ Modules/Product/Routes/product_routes.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Product routes
+|--------------------------------------------------------------------------
+|
+| Here you can see product routes.
+|
+*/
+
 Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
     $router->resource('products', 'ProductController', ['except' => 'show']);
 });
