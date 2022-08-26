c51212e21dc6e09d3247177707525f6558c09870
miladev-ent <milwad.dev@gmail.com>
2022-08-26 13:59:03 +0430

    add advertising test into phpunit xml file

---
+++ Modules/Advertising/Tests/Feature/AdvertisingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Advertising\Tests\Feature;
+
+use Tests\TestCase;
+
+class AdvertisingTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
