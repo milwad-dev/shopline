3a8d115830d887daa86210004389ea063b662f3c
miladev-ent <milwad.dev@gmail.com>
2022-08-28 17:11:10 +0430

    add phpdoc to advertising repository

--- Modules/Advertising/Repositories/AdvertisingRepoEloquent.php
+++ Modules/Advertising/Repositories/AdvertisingRepoEloquent.php
@@ -6,21 +6,43 @@ use Modules\Advertising\Models\Advertising;
 
 class AdvertisingRepoEloquent implements AdvertisingRepoEloquentInterface
 {
+    /**
+     * Get latest advertising (builder).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function getLatest()
     {
         return $this->query()->latest();
     }
 
-    public function findById($id)
+    /**
+     * Find or fail advertising by id.
+     *
+     * @param  int $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function findById(int $id)
     {
         return $this->query()->findOrFail($id);
     }
 
-    public function delete($id)
+    /**
+     * Delete advertising by id.
+     *
+     * @param  int $id
+     * @return mixed
+     */
+    public function delete(int $id)
     {
         return $this->query()->where('id', $id)->delete();
     }
 
+    /**
+     * Get query model (builder).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     private function query()
     {
         return Advertising::query();
