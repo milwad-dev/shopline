d68884bf7e3b4cad94e70178b5c780ab59995018
miladev-ent <milwad.dev@gmail.com>
2022-08-28 17:18:26 +0430

    add phpdoc to advertising service provider

--- Modules/Advertising/Providers/AdvertisingServiceProvider.php
+++ Modules/Advertising/Providers/AdvertisingServiceProvider.php
@@ -14,13 +14,53 @@ use Modules\Advertising\Services\AdvertisingServiceInterface;
 
 class AdvertisingServiceProvider extends ServiceProvider
 {
+    /**
+     * Get namespace for advertising controllers.
+     *
+     * @var string
+     */
     private string $namespace = 'Modules\Advertising\Http\Controllers';
+
+    /**
+     * Get migration path.
+     *
+     * @var string
+     */
     private string $migrationPath = '/../Database/Migrations';
+
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
     private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
     private string $routePath = '/../Routes/advertising_routes.php';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
     private string $name = 'Advertising';
+
+    /**
+     * Get route middleware.
+     *
+     * @var array|string[]
+     */
     private array $routeMiddleware = ['web', 'verify'];
 
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->loadPolicyFiles();
@@ -32,31 +72,61 @@ class AdvertisingServiceProvider extends ServiceProvider
         $this->bindService();
     }
 
-    private function loadPolicyFiles(): void
-    {
-        Gate::policy(Advertising::class, AdvertisingPolicy::class);
-    }
-
+    /**
+     * bind repository into interface.
+     *
+     * @return void
+     */
     private function bindRepository(): void
     {
         $this->app->bind(AdvertisingRepoEloquentInterface::class, AdvertisingRepoEloquent::class);
     }
 
+    /**
+     * bind service into interface.
+     *
+     * @return void
+     */
     private function bindService()
     {
         $this->app->bind(AdvertisingServiceInterface::class, AdvertisingService::class);
     }
 
+    /**
+     * Load policy files.
+     *
+     * @return void
+     */
+    private function loadPolicyFiles(): void
+    {
+        Gate::policy(Advertising::class, AdvertisingPolicy::class);
+    }
+
+    /**
+     * Load migration files.
+     *
+     * @return void
+     */
     private function loadMigrationFiles(): void
     {
         $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
     }
 
+    /**
+     * Load view files.
+     *
+     * @return void
+     */
     private function loadViewFiles(): void
     {
         $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
     }
 
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
     private function loadRouteFiles(): void
     {
         Route::middleware($this->routeMiddleware)
