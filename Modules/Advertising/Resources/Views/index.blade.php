f6df8b62225f3001ced91c76c4eacbe06c2fa155
miladev-ent <milwad.dev@gmail.com>
2022-08-28 17:42:18 +0430

    complete edit advertising

--- Modules/Advertising/Resources/Views/index.blade.php
+++ Modules/Advertising/Resources/Views/index.blade.php
@@ -16,61 +16,69 @@
                             <div class="table-responsive">
                                 <table class="table table-hover">
                                     <thead>
-                                    <tr class="text-center">
-                                        <th>#</th>
-                                        <th>Title</th>
-                                        <th>Link</th>
-                                        <th>Location</th>
-                                        <th>Status</th>
-                                        <th>User</th>
-                                        <th>Created At</th>
-                                        <th>Actions</th>
-                                    </tr>
+                                        <tr class="text-center">
+                                            <th>#</th>
+                                            <th>Image</th>
+                                            <th>Title</th>
+                                            <th>Link</th>
+                                            <th>Location</th>
+                                            <th>Status</th>
+                                            <th>User</th>
+                                            <th>Created At</th>
+                                            <th>Actions</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach ($advertisings as $advertising)
-                                        <tr class="text-center">
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>
-                                                {{ $advertising->title ?? 'No title' }}
-                                            </td>
-                                            <td>
-                                                <a href="{{ $advertising->getLink() }}">
-                                                    {{ $advertising->link ?? 'No link' }}
-                                                </a>
-                                            </td>
-                                            <td>
-                                                {{ $advertising->location }}
-                                            </td>
-                                            <td>
-                                                <span class="badge rounded-pill badge-light-{{ $advertising->getCssClassStatus() }} me-1">
-                                                    {{ $advertising->status }}
-                                                </span>
-                                            </td>
-                                            <td>
-                                                {{ $advertising->user->name }}
-                                            </td>
-                                            <td>{{ Carbon\Carbon::make($advertising->created_at)->format('Y-m-d') }}</td>
-                                            <td>
-                                                <div class="dropdown">
-                                                    <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0 waves-effect waves-float waves-light" data-bs-toggle="dropdown">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
-                                                    </button>
-                                                    <div class="dropdown-menu dropdown-menu-end">
-                                                        <a class="dropdown-item" href="{{ route('advertisings.edit', $advertising->id) }}">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 me-50"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
-                                                            <span>Edit</span>
+                                        @foreach ($advertisings as $advertising)
+                                            <tr class="text-center">
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>
+                                                    <img src="{{ $advertising->media->thumb }}" width="80" class="img-thumbnail">
+                                                </td>
+                                                <td>
+                                                    {{ $advertising->title ?? 'No title' }}
+                                                </td>
+                                                <td>
+                                                    @if ($advertising->link)
+                                                        <a href="{{ $advertising->getLink() }}">
+                                                            {{ $advertising->link }}
                                                         </a>
-                                                        <a class="dropdown-item"
-                                                           onclick="deleteItem(event, '{{ route('advertisings.destroy', $advertising->id) }}')">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash me-50"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-                                                            <span>Delete</span>
-                                                        </a>
+                                                    @else
+                                                        No link
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    {{ $advertising->location }}
+                                                </td>
+                                                <td>
+                                                    <span class="badge rounded-pill badge-light-{{ $advertising->getCssClassStatus() }} me-1">
+                                                        {{ $advertising->status }}
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    {{ $advertising->user->name }}
+                                                </td>
+                                                <td>{{ Carbon\Carbon::make($advertising->created_at)->format('Y-m-d') }}</td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0 waves-effect waves-float waves-light" data-bs-toggle="dropdown">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-end">
+                                                            <a class="dropdown-item" href="{{ route('advertisings.edit', $advertising->id) }}">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 me-50"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
+                                                                <span>Edit</span>
+                                                            </a>
+                                                            <a class="dropdown-item"
+                                                               onclick="deleteItem(event, '{{ route('advertisings.destroy', $advertising->id) }}')">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash me-50"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                                                                <span>Delete</span>
+                                                            </a>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                                 {{ $advertisings->links() }}
