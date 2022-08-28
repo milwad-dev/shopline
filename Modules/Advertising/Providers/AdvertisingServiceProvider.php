637c65d4c9d23f8b67687d3ab430cb2fb7b3d36a
miladev-ent <milwad.dev@gmail.com>
2022-08-28 17:15:23 +0430

    add variable for path for advertising service provider

--- Modules/Advertising/Providers/AdvertisingServiceProvider.php
+++ Modules/Advertising/Providers/AdvertisingServiceProvider.php
@@ -14,7 +14,12 @@ use Modules\Advertising\Services\AdvertisingServiceInterface;
 
 class AdvertisingServiceProvider extends ServiceProvider
 {
-    public string $namespace = 'Modules\Advertising\Http\Controllers';
+    private string $namespace = 'Modules\Advertising\Http\Controllers';
+    private string $migrationPath = '/../Database/Migrations';
+    private string $viewPath = '/../Resources/Views/';
+    private string $routePath = '/../Routes/advertising_routes.php';
+    private string $name = 'Advertising';
+    private array $routeMiddleware = ['web', 'verify'];
 
     public function register()
     {
@@ -44,18 +49,18 @@ class AdvertisingServiceProvider extends ServiceProvider
 
     private function loadMigrationFiles(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
     }
 
     private function loadViewFiles(): void
     {
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Advertising');
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
     }
 
     private function loadRouteFiles(): void
     {
-        Route::middleware(['web', 'verify'])
+        Route::middleware($this->routeMiddleware)
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../Routes/advertising_routes.php');
+            ->group(__DIR__ . $this->routePath);
     }
 }
