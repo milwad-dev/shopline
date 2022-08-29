0f44b56bcc63f918081c0966f913cf6fd8da40e2
miladev-ent <milwad.dev@gmail.com>
2022-08-30 00:43:33 +0430

    part part main page

---
+++ Modules/Home/Resources/Views/Home/parts/sellings.blade.php
@@ -0,0 +1,266 @@
+<section class="section-padding mb-30">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0">
+                <h4 class="section-title style-1 mb-30 animated animated">Top Selling</h4>
+                <div class="product-list-small animated animated">
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-1.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Nestle Original Coffee-Mate Coffee Creamer</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-2.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Nestle Original Coffee-Mate Coffee Creamer</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-3.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Nestle Original Coffee-Mate Coffee Creamer</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-md-0">
+                <h4 class="section-title style-1 mb-30 animated animated">Trending Products</h4>
+                <div class="product-list-small animated animated">
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-4.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Organic Cage-Free Grade A Large Brown Eggs</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-5.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Seeds of Change Organic Quinoa, Brown, & Red Rice</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-6.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Naturally Flavored Cinnamon Vanilla Light Roast Coffee</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-lg-block">
+                <h4 class="section-title style-1 mb-30 animated animated">Recently added</h4>
+                <div class="product-list-small animated animated">
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-7.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Pepperidge Farm Farmhouse Hearty White Bread</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-8.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Organic Frozen Triple Berry Blend</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-9.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Oroweat Country Buttermilk Bread</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-xl-block">
+                <h4 class="section-title style-1 mb-30 animated animated">Top Rated</h4>
+                <div class="product-list-small animated animated">
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-10.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Foster Farms Takeout Crispy Classic Buffalo Wings</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-11.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">Angie’s Boomchickapop Sweet & Salty Kettle Corn</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="row align-items-center hover-up">
+                        <figure class="col-md-4 mb-0">
+                            <a href="shop-product-right.html"><img src="assets/imgs/shop/thumbnail-12.jpg" alt="" /></a>
+                        </figure>
+                        <div class="col-md-8 mb-0">
+                            <h6>
+                                <a href="shop-product-right.html">All Natural Italian-Style Chicken Meatballs</a>
+                            </h6>
+                            <div class="product-rate-cover">
+                                <div class="product-rate d-inline-block">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <span class="font-small ml-5 text-muted"> (4.0)</span>
+                            </div>
+                            <div class="product-price">
+                                <span>$32.85</span>
+                                <span class="old-price">$33.8</span>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
