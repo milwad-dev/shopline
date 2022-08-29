f7003c47fb9a432935efd79e6cea104a77db44ca
miladev-ent <milwad.dev@gmail.com>
2022-08-29 22:26:36 +0430

    register policy in product service provider

--- Modules/Product/Providers/ProductServiceProvider.php
+++ Modules/Product/Providers/ProductServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Modules\Product\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\Product\Models\Product;
+use Modules\Product\Policies\ProductPolicy;
 use Modules\Product\Repositories\ProductRepoEloquent;
 use Modules\Product\Repositories\ProductRepoEloquentInterface;
 
@@ -16,6 +19,7 @@ class ProductServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Product');
 
+        Gate::policy(Product::class, ProductPolicy::class);
         Route::middleware(['web', 'verify'])->namespace($this->namespace)
         ->group(__DIR__ . '/../Routes/product_routes.php');
         $this->bindRepository();
