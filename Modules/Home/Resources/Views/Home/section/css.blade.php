c924f05f9d38f64e56b3e4de5a64ae5ca5848dc0
miladev-ent <milwad.dev@gmail.com>
2022-08-30 00:10:01 +0430

    remove Home folder from home module

--- Modules/Home/Resources/Views/Home/section/css.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- Favicon --}}
-<link rel="shortcut icon" type="image/x-icon" href="{{ asset('home/imgs/theme/favicon.svg') }}" />
-
-{{-- Css --}}
-<link rel="stylesheet" href="{{ asset('home/css/main.css') }}" />
-@yield('css')
--- Modules/Home/Resources/Views/Home/section/meta.blade.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8" />
-<meta http-equiv="x-ua-compatible" content="ie=edge" />
-<meta name="description" content="" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta property="og:title" content="" />
-<meta property="og:type" content="" />
-<meta property="og:url" content="" />
-<meta property="og:image" content="" />
-@yield('meta')
