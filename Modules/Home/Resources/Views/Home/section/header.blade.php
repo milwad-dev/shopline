b6d2ed25576f4a74213c9f0615aba973692c0238
miladev-ent <milwad.dev@gmail.com>
2022-09-01 23:53:43 +0430

    complete header categories

--- Modules/Home/Resources/Views/Home/section/header.blade.php
+++ Modules/Home/Resources/Views/Home/section/header.blade.php
@@ -239,89 +239,30 @@
                                                 Products
                                             </a>
                                         </li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link" href="javascript:void(0)">
-                                                About-us
-                                            </a>
-                                        </li>
                                         <li class="nav-item dropdown dropdown-mega">
                                             <a class="nav-link dropdown-toggle ps-xl-2 ps-0" href="javascript:void(0)"
                                                data-bs-toggle="dropdown">Categories
                                             </a>
                                             <div class="dropdown-menu dropdown-menu-2 row g-3">
-                                                <div class="dropdown-column col-xl-3">
-                                                    <h5 class="dropdown-header">Daily Vegetables</h5>
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Beans &
-                                                        Brinjals</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Broccoli
-                                                        & Cauliflower</a>
-
-                                                    <a href="shop-left-sidebar.html" class="dropdown-item">Chilies,
-                                                        Garlic</a>
-
-                                                    <a class="dropdown-item"
-                                                       href="shop-left-sidebar.html">Vegetables & Salads</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Gourd,
-                                                        Cucumber</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Herbs &
-                                                        Sprouts</a>
-
-                                                    <a href="demo-personal-portfolio.html"
-                                                       class="dropdown-item">Lettuce
-                                                        & Leafy</a>
-                                                </div>
-
-                                                <div class="dropdown-column col-xl-3">
-                                                    <h5 class="dropdown-header">Baby Tender</h5>
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Beans &
-                                                        Brinjals</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Broccoli
-                                                        & Cauliflower</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Chilies,
-                                                        Garlic</a>
-
-                                                    <a class="dropdown-item"
-                                                       href="shop-left-sidebar.html">Vegetables & Salads</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Gourd,
-                                                        Cucumber</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Potatoes
-                                                        & Tomatoes</a>
-
-                                                    <a href="shop-left-sidebar.html" class="dropdown-item">Peas &
-                                                        Corn</a>
-                                                </div>
-
-                                                <div class="dropdown-column col-xl-3">
-                                                    <h5 class="dropdown-header">Exotic Vegetables</h5>
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Asparagus
-                                                        & Artichokes</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Avocados
-                                                        & Peppers</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Broccoli
-                                                        & Zucchini</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Celery,
-                                                        Fennel & Leeks</a>
-
-                                                    <a class="dropdown-item" href="shop-left-sidebar.html">Chilies &
-                                                        Lime</a>
-                                                </div>
-
-                                                <div class="dropdown-column dropdown-column-img col-3"></div>
+                                                @foreach ($categories->chunk(7) as $category)
+                                                    <div class="dropdown-column col-xl-3">
+                                                        @foreach ($category as $cat)
+                                                            <a class="dropdown-item" href="{{ $cat->path() }}">
+                                                                {{ $cat->title }}
+                                                            </a>
+                                                        @endforeach
+                                                    </div>
+                                                @endforeach
                                             </div>
                                         </li>
                                         <li class="nav-item dropdown">
                                             <a class="nav-link" href="javascript:void(0)">
                                                 Blog
+                                            </a>
+                                        </li>
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link" href="javascript:void(0)">
+                                                About-us
                                             </a>
                                         </li>
                                         <li class="nav-item">
