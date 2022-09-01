3844a858c308ce34c56019e5edb0f43e7d78e9ec
miladev-ent <milwad.dev@gmail.com>
2022-09-01 23:59:00 +0430

    add discount to specific file

---
+++ Modules/Home/Resources/Views/Home/parts/discount.blade.php
@@ -0,0 +1,19 @@
+<section>
+    <div class="container-fluid-lg">
+        <div class="row">
+            <div class="col-12">
+                <div class="banner-contain">
+                    <div class="banner-contain hover-effect">
+                        <img src="{{ asset('home/images/vegetable/banner/15.jpg') }}" class="bg-img blur-up lazyload" alt="">
+                        <div class="banner-details p-center p-sm-4 p-3 text-white text-center">
+                            <div>
+                                <h3 class="lh-base fw-bold text-light">Get $3 Cashback! Min Order of $30</h3>
+                                <h6 class="coupon-code">Use Code : GROCERY1920</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- Modules/Home/Resources/Views/index.blade.php
+++ Modules/Home/Resources/Views/index.blade.php
@@ -8,27 +8,7 @@
          'adv' => $homeRepo->getOneLatestAdvByLocation(Modules\Advertising\Enums\AdvertisingLocationEnum::LOCATION_SLIDER->value)->first()
      ])
     @include('Home::Home.parts.categories')
-    <!-- Discount Section Start -->
-    <section>
-        <div class="container-fluid-lg">
-            <div class="row">
-                <div class="col-12">
-                    <div class="banner-contain">
-                        <div class="banner-contain hover-effect">
-                            <img src="../assets/images/vegetable/banner/15.jpg" class="bg-img blur-up lazyload" alt="">
-                            <div class="banner-details p-center p-sm-4 p-3 text-white text-center">
-                                <div>
-                                    <h3 class="lh-base fw-bold text-light">Get $3 Cashback! Min Order of $30</h3>
-                                    <h6 class="coupon-code">Use Code : GROCERY1920</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Discount Section End -->
+    @include('Home::Home.parts.discount')
 
     <!-- Product Section Start -->
     <section>
