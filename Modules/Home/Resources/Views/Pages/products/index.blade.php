f7fea55db9ccaef6f90becfdd1adcc080d7c4131
miladev-ent <milwad.dev@gmail.com>
2022-09-08 00:04:00 +0430

    remove breadcrumb

--- Modules/Home/Resources/Views/Pages/products/index.blade.php
+++ Modules/Home/Resources/Views/Pages/products/index.blade.php
@@ -3,27 +3,6 @@
 @section('title', 'Products')
 
 @section('content')
-    <section class="breadscrumb-section pt-0">
-        <div class="container-fluid-lg">
-            <div class="row">
-                <div class="col-12">
-                    <div class="breadscrumb-contain">
-                        <h2>Products</h2>
-                        <nav>
-                            <ol class="breadcrumb mb-0">
-                                <li class="breadcrumb-item">
-                                    <a href="{{ route('home.index') }}">
-                                        <i class="fa-solid fa-house"></i>
-                                    </a>
-                                </li>
-                                <li class="breadcrumb-item active" aria-current="page">Products</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     <section>
         <div class="container-fluid-lg">
             <div class="row">
