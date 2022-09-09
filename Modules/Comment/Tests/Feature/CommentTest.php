dfc0503c110b7dbc1ae81f4ca52906f498e7332e
miladev-ent <milwad.dev@gmail.com>
2022-09-10 00:40:12 +0430

    make & register comment test

---
+++ Modules/Comment/Tests/Feature/CommentTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Comment\Tests\Feature;
+
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
