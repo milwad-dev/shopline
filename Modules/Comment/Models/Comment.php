545a5f59a81fc0d018d310af50dd1b26fae04799
miladev-ent <milwad.dev@gmail.com>
2022-09-10 00:26:45 +0430

    move comments file into module

---
+++ Modules/Comment/Models/Comment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Comment\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+}
---
+++ Modules/Comment/Policies/CommentPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Comment\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CommentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
