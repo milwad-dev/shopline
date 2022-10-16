c37c5f194551f96ce3d2e3ca3bbe3015105c6584
miladev-ent <milwad.dev@gmail.com>
2022-10-16 19:12:13 +0330

    set up contact tests

--- Modules/Contact/Models/Contact.php
+++ Modules/Contact/Models/Contact.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
+    /**
+     * Fillable columns.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['name', 'email', 'phone', 'sujbect', 'message', 'is_read'];
 }
---
+++ Modules/Contact/Tests/Feature/ContactTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Contact\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContactTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
