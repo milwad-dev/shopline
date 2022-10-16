05a8472c333e76af916e9c062a441fb0cca47664
miladev-ent <milwad.dev@gmail.com>
2022-10-16 19:51:22 +0330

    add contact factory

---
+++ Modules/Share/Database/Factories/ContactFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Share\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Contact\Models\Contact;
+
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'phone' => "09103400042",
+            'sujbect' => $this->faker->title,
+            'message' => $this->faker->text,
+        ];
+    }
+}
