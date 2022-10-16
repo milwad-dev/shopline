84b6f3be7c5ea542e53b4e5ec3b5e9934be57c27
miladev-ent <milwad.dev@gmail.com>
2022-10-16 21:11:36 +0330

    add contact route

--- Modules/Home/Routes/home_routes.php
+++ Modules/Home/Routes/home_routes.php
@@ -12,18 +12,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group([], static function ($router) {
-    // Home
+    # Home
     $router->get('/', ['uses' => 'Home\HomeController@index', 'as' => 'home.index']);
     $router->get('comming-soon', ['uses' => 'Home\HomeController@commingSoon', 'as' => 'comming-soon']);
 
-    // Products
+    # Products
     $router->get('products', ['uses' => 'Product\ProductController@index', 'as' => 'products.home']);
     $router->get('products/{sku}/d/{slug}', ['uses' => 'Product\ProductController@details', 'as' => 'products.details']);
 
-    // Blogs
+    # Blogs
     $router->get('blog', ['uses' => 'Blog\BlogController@index', 'as' => 'blog.home']);
     $router->get('blog/{slug}', ['uses' => 'Blog\BlogController@details', 'as' => 'blog.details']);
 
-    // Comments
+    # Comments
     $router->post('comments', ['uses' => 'Comment\CommentController@store', 'as' => 'comments.store']);
+    
+    # Contacts
+    $router->get('contacts', ['uses' => 'Contact\ContactController@create', 'as' => 'contacts.create']);
+    $router->post('contacts', ['uses' => 'Contact\ContactController@store', 'as' => 'contacts.store']);
 });
