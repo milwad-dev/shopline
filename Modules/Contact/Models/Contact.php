e512a227dc48c0945d64b267af7b8c8567edb53d
miladev-ent <milwad.dev@gmail.com>
2022-10-16 19:36:30 +0330

    add scopes into contact model

--- Modules/Contact/Models/Contact.php
+++ Modules/Contact/Models/Contact.php
@@ -15,4 +15,28 @@ class Contact extends Model
      * @var string[]
      */
     protected $fillable = ['name', 'email', 'phone', 'sujbect', 'message', 'is_read'];
+
+    # Scopes
+
+    /**
+     * Scope is_read is true.
+     *
+     * @param  $query
+     * @return mixed
+     */
+    public function scopeRead($query)
+    {
+        return $query->where('is_read', 1);
+    }
+
+    /**
+     * Scope is_read is false.
+     *
+     * @param  $query
+     * @return mixed
+     */
+    public function scopeNotRead($query)
+    {
+        return $query->where('is_read', 0);
+    }
 }
