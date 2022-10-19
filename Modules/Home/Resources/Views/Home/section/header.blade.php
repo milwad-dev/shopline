0d1c5b633af3fa8622aeb1bfb28f719d6efb4e65
miladev-ent <milwad.dev@gmail.com>
2022-10-19 22:36:17 +0330

    correct cart count

--- Modules/Home/Resources/Views/Home/section/header.blade.php
+++ Modules/Home/Resources/Views/Home/section/header.blade.php
@@ -124,7 +124,8 @@
                                     <div class="onhover-dropdown header-badge">
                                         <button type="button" class="btn p-0 position-relative header-wishlist">
                                             <i data-feather="shopping-cart"></i>
-                                            <span class="position-absolute top-0 start-100 translate-middle badge">2
+                                            <span class="position-absolute top-0 start-100 translate-middle badge">
+                                                {{ count(session()->get('cart')) }}
                                                 <span class="visually-hidden">unread messages</span>
                                             </span>
                                         </button>
