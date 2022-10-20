53f6fa2612c0ca5b69a56c4e0aae8c06fc577b7c
miladev-ent <milwad.dev@gmail.com>
2022-10-20 22:38:24 +0330

    add phpdoc into cart controller

--- Modules/Cart/Http/Controllers/CartController.php
+++ Modules/Cart/Http/Controllers/CartController.php
@@ -15,6 +15,14 @@ class CartController extends Controller
         $this->service = $cartService;
     }
 
+    /**
+     * Add product into session by product id & show success messag with redirect.
+     *
+     * @param  $productId
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function add($productId)
     {
         $this->service->add($productId);
@@ -22,6 +30,14 @@ class CartController extends Controller
         return $this->sucessMessageWithRedirect('Add to cart successfully');
     }
 
+    /**
+     * Delete product from session by product id.
+     *
+     * @param  $productId
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function delete($productId)
     {
         $this->service->remove($productId);
@@ -29,6 +45,11 @@ class CartController extends Controller
         return $this->sucessMessageWithRedirect('Remove item from cart successfully');
     }
 
+    /**
+     * Delete all products from cart.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function deleteAll()
     {
         $this->service->removeAll();
@@ -36,6 +57,14 @@ class CartController extends Controller
         return $this->sucessMessageWithRedirect('All item deleted from cart successfully');
     }
 
+    # Private methods
+
+    /**
+     * Show success message with redirect.
+     *
+     * @param  string $title
+     * @return \Illuminate\Http\RedirectResponse
+     */
     private function sucessMessageWithRedirect(string $title): \Illuminate\Http\RedirectResponse
     {
         ShareService::successToast($title);
