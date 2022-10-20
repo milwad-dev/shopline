523ea1cd20da5844a354a05c9dd0d2e9b9d9f182
miladev-ent <milwad.dev@gmail.com>
2022-10-20 22:35:26 +0330

    add phpdoc into cart service provider & delete cart repo

--- Modules/Cart/Providers/CartServiceProvider.php
+++ Modules/Cart/Providers/CartServiceProvider.php
@@ -8,14 +8,35 @@ use Illuminate\Support\ServiceProvider;
 class CartServiceProvider extends ServiceProvider
 {
     /**
+     * Get namespace for cart controllers.
+     *
+     * @var string
+     */
+    private string $namespace = 'Modules\Cart\Http\Controllers';
+
+    /**
+     * Get route middleware.
+     *
+     * @var array|string[]
+     */
+    private array $routeMiddleware = ['web', 'verify'];
+
+    /**
+     * Get cart path.
+     *
+     * @var string
+     */
+    private string $cartPath = '/../Routes/cart_routes.php';
+
+    /**
      * Register files.
      *
      * @return void
      */
     public function register()
     {
-        Route::middleware(['web', 'verify'])
-            ->namespace('Modules\Cart\Http\Controllers')
-            ->group(__DIR__ . '/../Routes/cart_routes.php');
+        Route::middleware($this->routeMiddleware)
+            ->namespace($this->namespace)
+            ->group(__DIR__ . $this->cartPath);
     }
 }
--- Modules/Cart/Repositories/CartRepoEloquent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Modules\Cart\Repositories;
-
-class CartRepoEloquent
-{
-    //
-}
