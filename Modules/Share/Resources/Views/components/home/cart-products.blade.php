2b7f48275f252f54af9628591541a00619c8f1d2
miladev-ent <milwad.dev@gmail.com>
2022-10-20 19:54:56 +0330

    correct delete cart icon style

--- Modules/Share/Resources/Views/components/home/cart-products.blade.php
+++ Modules/Share/Resources/Views/components/home/cart-products.blade.php
@@ -2,17 +2,16 @@
     <div class="drop-cart">
         <a href="{{ route('products.details', ['sku' => $product['sku'], 'slug' => $product['slug']]) }}" class="drop-image">
             <img src="{{ $product['first-media'] }}"
-                 class="blur-up lazyload" alt="">
+                 class="blur-up lazyload" alt="product image">
         </a>
-
         <div class="drop-contain">
             <a href="{{ route('products.details', ['sku' => $product['sku'], 'slug' => $product['slug']]) }}">
                 <h5>{{ $product['title'] }}</h5>
             </a>
             <h6><span>{{ $product['quantity'] }} x</span> {{ $product['price'] }}</h6>
-            <button class="close-button close_button">
-                <i class="fa-solid fa-xmark"></i>
-            </button>
+            <a class="close-button close_button">
+                <i data-feather="x-circle"></i>
+            </a>
         </div>
     </div>
 </li>
