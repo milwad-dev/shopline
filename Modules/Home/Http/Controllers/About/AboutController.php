6e86f1fc54b312331ed344126894bde008c78243
miladev-ent <milwad.dev@gmail.com>
2022-10-26 21:37:49 +0330

    add about controller

---
+++ Modules/Home/Http/Controllers/About/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Home\Http\Controllers\About;
+
+use Modules\Share\Http\Controllers\Controller;
+
+class AboutController extends Controller
+{
+    /**
+     * Show about-us view page.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function __invoke()
+    {
+        return view('Home::Pages.about-us.home');
+    }
+}
