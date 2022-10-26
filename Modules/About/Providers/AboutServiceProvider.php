1190b2eb0a50f113de732a40d3eb76a2c91847d3
miladev-ent <milwad.dev@gmail.com>
2022-10-26 22:26:03 +0330

    add method for register about files

--- Modules/About/Providers/AboutServiceProvider.php
+++ Modules/About/Providers/AboutServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Modules\About\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Modules\About\Models\About;
+use Modules\About\Policies\Policies\AboutPolicy;
 
 class AboutServiceProvider extends ServiceProvider
 {
@@ -11,10 +14,31 @@ class AboutServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'About');
+        $this->loadMigrationFiles();
+        $this->loadViewFiles();
+        $this->loadRouteFiles();
+        $this->loadPolicyFiles();
+    }
 
-        Route::middleware(['web', 'verify'])->namespace($this->namespace)
-        ->group(__DIR__ . '/../Routes/about_routes.php');
+    private function loadMigrationFiles(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'About');
+    }
+
+    private function loadRouteFiles(): void
+    {
+        Route::middleware(['web', 'verify'])
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../Routes/about_routes.php');
+    }
+
+    private function loadPolicyFiles(): void
+    {
+        Gate::policy(About::class, AboutPolicy::class);
     }
 }
