7c12735542277593e1a9ef943b18953949be735e
miladev-ent <milwad.dev@gmail.com>
2022-10-26 22:47:53 +0330

    complete about service

--- Modules/About/Services/AboutService.php
+++ Modules/About/Services/AboutService.php
@@ -6,23 +6,36 @@ use Modules\About\Models\About;
 
 class AboutService
 {
-    public function store($request)
+    /**
+     * Store about by array of data.
+     *
+     * @param  array $data
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model
+     */
+    public function store(array $data)
     {
-        return $this->query()->create([
-
-        ]);
+        return $this->query()->create(['body' => $data['body']]);
     }
 
-    public function update($request, $id)
+    /**
+     * Update about by id & array of data.
+     *
+     * @param  array $data
+     * @param  $id
+     * @return int
+     */
+    public function update(array $data, $id)
     {
-        return $this->query()->whereId($id)->update([
-
-        ]);
+        return $this->query()->where('id', $id)->update(['body' => $data['body']]);
     }
 
+    /**
+     * Get query model(builder).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     private function query()
     {
         return About::query();
     }
 }
-        
