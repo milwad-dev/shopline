d757d6286c633c3c7666bd099b3bae9e6ee87517
miladev-ent <milwad.dev@gmail.com>
2022-10-26 22:30:38 +0330

    add phpdoc into about service provider

--- Modules/About/Providers/AboutServiceProvider.php
+++ Modules/About/Providers/AboutServiceProvider.php
@@ -10,14 +10,53 @@ use Modules\About\Policies\Policies\AboutPolicy;
 
 class AboutServiceProvider extends ServiceProvider
 {
+    /**
+     * Get namespace for controllers.
+     *
+     * @var string
+     */
     private string $namespace = 'Modules\About\Http\Controllers';
 
+    /**
+     * Get migration path.
+     *
+     * @var string
+     */
     private string $migrationPath = '/../Database/Migrations';
+
+    /**
+     * Get view path.
+     *
+     * @var string
+     */
     private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Get name.
+     *
+     * @var string
+     */
     private string $name = 'About';
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
     private string $routePath = '/../Routes/about_routes.php';
+
+    /**
+     * Get route middleware.
+     *
+     * @var array|string[]
+     */
     private array  $routeMiddleware = ['web', 'verify'];
 
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->loadMigrationFiles();
@@ -26,16 +65,31 @@ class AboutServiceProvider extends ServiceProvider
         $this->loadPolicyFiles();
     }
 
+    /**
+     * Load migration files.
+     *
+     * @return void
+     */
     private function loadMigrationFiles(): void
     {
         $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
     }
 
+    /**
+     * Load view files.
+     *
+     * @return void
+     */
     private function loadViewFiles(): void
     {
         $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->name);
     }
 
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
     private function loadRouteFiles(): void
     {
         Route::middleware($this->routeMiddleware)
@@ -43,6 +97,11 @@ class AboutServiceProvider extends ServiceProvider
             ->group(__DIR__ . $this->routePath);
     }
 
+    /**
+     * Load policy files.
+     *
+     * @return void
+     */
     private function loadPolicyFiles(): void
     {
         Gate::policy(About::class, AboutPolicy::class);
