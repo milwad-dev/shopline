a2088ad97525c5a0dfc4c27b0ad715b9dd86e505
miladev-ent <milwad.dev@gmail.com>
2022-10-26 22:35:06 +0330

    add about routes

--- Modules/About/Routes/about_routes.php
+++ Modules/About/Routes/about_routes.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function ($router) {
+/*
+|--------------------------------------------------------------------------
+| About routes
+|--------------------------------------------------------------------------
+|
+| Here you can see about routes.
+|
+*/
 
+Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
+    $router->resource('abouts', 'AboutController', ['except' => ['show', 'destroy']]);
 });
-        
