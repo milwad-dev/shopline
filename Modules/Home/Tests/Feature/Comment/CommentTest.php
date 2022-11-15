8ac1acae6c7747bfc8d55ea366e131a305adcbcf
miladev-ent <milwad.dev@gmail.com>
2022-11-15 20:11:50 +0330

    add login user can store comment test

--- Modules/Home/Tests/Feature/Comment/CommentTest.php
+++ Modules/Home/Tests/Feature/Comment/CommentTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Modules\Comment\Models\Comment;
 use Modules\Product\Models\Product;
+use Modules\RolePermission\Database\Seeds\PermissionSeeder;
+use Modules\User\Models\User;
 use Tests\TestCase;
 
 class CommentTest extends TestCase
@@ -41,5 +43,32 @@ class CommentTest extends TestCase
         $this->assertDatabaseMissing($this->tableName, ['body' => $body]);
         $this->assertDatabaseCount($this->tableName, 0);
         $this->assertEquals(0, Comment::query()->count());
+    }
+
+    /**
+     * Test login user can store comment.
+     *
+     * @test
+     * @return void
+     */
+    public function login_user_can_store_comment()
+    {
+        $this->seed(PermissionSeeder::class);
+        auth()->login(User::factory()->create());
+
+        $product = Product::factory()->create();
+        $body = $this->faker->text;
+
+        $response = $this->post(route('comments.store'), [
+            'body' => $body,
+            'commentable_id' => (string) $product->id,
+            'commentable_type' => get_class($product),
+        ]);
+        $response->assertRedirect();
+        $response->assertSessionHas('alert');
+
+        $this->assertDatabaseHas($this->tableName, ['body' => $body]);
+        $this->assertDatabaseCount($this->tableName, 1);
+        $this->assertEquals(1, Comment::query()->count());
     }
 }
