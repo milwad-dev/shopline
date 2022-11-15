6862442b433e74147e1586f783420cc055d83764
miladev-ent <milwad.dev@gmail.com>
2022-11-15 19:00:55 +0330

    complete blog details page tests

--- Modules/Home/Tests/Feature/Blog/BlogTest.php
+++ Modules/Home/Tests/Feature/Blog/BlogTest.php
@@ -2,11 +2,15 @@
 
 namespace Modules\Home\Tests\Feature\Blog;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Article\Models\Article;
 use Modules\User\Models\User;
 use Tests\TestCase;
 
 class BlogTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * Test guest user can see blog index page.
      *
@@ -33,5 +37,33 @@ class BlogTest extends TestCase
         $this->get(route('blog.home'))
             ->assertViewIs('Home::Pages.blog.index')
             ->assertViewHas(['articles', 'randomArticles', 'categories']);
+    }
+
+    /**
+     * Test guest user can see blog details page.
+     *
+     * @test
+     * @return void
+     */
+    public function guest_user_can_see_blog_details_page()
+    {
+        $this->get(route('blog.details', Article::factory()->create()->slug))
+            ->assertViewIs('Home::Pages.blog.details')
+            ->assertViewHas(['article', 'randomArticles', 'categories']);
+    }
+
+    /**
+     * Test login user can see blog details page.
+     *
+     * @test
+     * @return void
+     */
+    public function login_user_can_see_blog_details_page()
+    {
+        auth()->login(User::factory()->create());
+
+        $this->get(route('blog.details', Article::factory()->create()->slug))
+            ->assertViewIs('Home::Pages.blog.details')
+            ->assertViewHas(['article', 'randomArticles', 'categories']);
     }
 }
