faf75488537855d8a3044e865059bfe2837d0545
miladev-ent <milwad.dev@gmail.com>
2022-11-18 19:39:49 +0330

    complete payment service

--- Modules/Payment/Services/PaymentService.php
+++ Modules/Payment/Services/PaymentService.php
@@ -2,27 +2,107 @@
 
 namespace Modules\Payment\Services;
 
+use Illuminate\Http\RedirectResponse;
+use Modules\Payment\Enums\PaymentStatusEnum;
 use Modules\Payment\Models\Payment;
+use Modules\User\Models\User;
 
 class PaymentService
 {
-    public function store($request)
-    {
-        return $this->query()->create([
+    /**
+     * Generate payments.
+     *
+     * @param  string $amount
+     * @param  object $paymentable
+     * @param  User $buyer
+     * @param  int|string|null $seller_id
+     * @param  array $discounts
+     * @return false|RedirectResponse|Payment
+     */
+    public static function generate(
+        string $amount,
+        object $paymentable,
+        User $buyer,
+        int|string $seller_id = null,
+        array $discounts = []
+    ) {
+        if ($amount <= 0 || is_null($paymentable->id) || is_null($buyer->id)) {
+            return false;
+        }
 
-        ]);
+        $gateway = resolve(Gateway::class);
+        $invoiceId = $gateway->request($amount, $paymentable->title);
+        if (is_array($invoiceId)) {
+            return back();
+        }
+
+        if (is_null($paymentable->percent)) {
+            $seller_p = $paymentable->percent;
+            $seller_share = ($amount / 100 * $seller_p);
+            $site_share = $amount - $seller_share;
+        } else {
+            $seller_p = $seller_share = 0;
+            $site_share = $amount;
+        }
+
+        return $this->store([
+            'buyer_id' => $buyer->id,
+            'seller_id' => $seller_id,
+            'paymentable_id' => $paymentable->id,
+            'paymentable_type' => get_class($paymentable),
+            'amount' => $amount,
+            'invoice_id' => $invoiceId,
+            'gateway' => $gateway->getName(),
+            'status' => PaymentStatusEnum::STATUS_PENDING->value,
+            'seller_p' => $seller_p,
+            'seller_share' => $seller_share,
+            'site_share' => $site_share,
+        ], $discounts);
     }
 
-    public function update($request, $id)
+    /**
+     * Store payments.
+     *
+     * @param  array $data
+     * @param  array $discounts
+     * @return Payment
+     */
+    private function store(array $data, array $discounts = [])
     {
-        return $this->query()->whereId($id)->update([
-
+        $payments = Payment::query()->create([
+            "buyer_id"          => $data['buyer_id'],
+            "paymentable_id"    => $data['paymentable_id'],
+            "paymentable_type"  => $data['paymentable_type'],
+            "amount"            => $data['amount'],
+            "seller_id"         => $data['seller_id'],
+            "invoice_id"        => $data['invoice_id'],
+            "gateway"           => $data['gateway'],
+            "status"            => $data['status'],
+            "seller_p"          => $data['seller_p'],
+            "seller_share"      => $data['seller_share'],
+            "site_share"        => $data['site_share'],
         ]);
+        $this->syncDiscountToPayments($payments, $discounts);
+
+        return $payments;
     }
 
-    private function query()
+    /**
+     * Sync discount to payments.
+     *
+     * @param  Payment $payments
+     * @param  array $discounts
+     * @return Payment
+     */
+    private function syncDiscountToPayments(Payment $payments, array $discounts = [])
     {
-        return Payment::query();
+        foreach ($discounts as $discount) {
+            $discountIds[] = $discount->id;
+        }
+        if (isset($discountIds)) {
+            $payments->discounts()->sync($discountIds);
+        }
+
+        return $payments;
     }
 }
-        
