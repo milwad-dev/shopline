bbfeac3230eb10d8f6ad5cf74a92654eb64d2c30
miladev-ent <milwad.dev@gmail.com>
2022-11-18 19:41:38 +0330

    complete payment route

--- Modules/Payment/Routes/payment_routes.php
+++ Modules/Payment/Routes/payment_routes.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function ($router) {
+/*
+|--------------------------------------------------------------------------
+| Payment routes
+|--------------------------------------------------------------------------
+|
+| Here you can see payments routes.
+|
+*/
 
+Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
+    $router->any('payments/callback', ['uses' => 'PaymentController@callback', 'as' => 'payment.callback']);
+    $router->get('payments', ['uses' => 'PaymentController@index', 'as' => 'payments.index']);
+    $router->get('purchases', ['uses' => 'PaymentController@purchases', 'as' => 'purchases.index']);
 });
-        
