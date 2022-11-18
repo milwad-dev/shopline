ff91b76a4c1f2ce9c4afdc4bd2e0b7755b1c5192
miladev-ent <milwad.dev@gmail.com>
2022-11-18 18:49:32 +0330

    make & move discount files

---
+++ Modules/Discount/Http/Controllers/DiscountController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Modules\Discount\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Modules\Discount\Discount;
+
+class DiscountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Modules\Discount\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Modules\Discount\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Modules\Discount\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Modules\Discount\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Discount $discount)
+    {
+        //
+    }
+}
---
+++ Modules/Discount/Models/Discount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Discount\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discount extends Model
+{
+    use HasFactory;
+}
