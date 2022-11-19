1578b65af50075cd8b386c003242c08b20b375af
miladev-ent <milwad.dev@gmail.com>
2022-11-19 18:31:32 +0330

    remove index & purchases function in payment controller

--- Modules/Payment/Http/Controllers/PaymentController.php
+++ Modules/Payment/Http/Controllers/PaymentController.php
@@ -22,7 +22,7 @@ class PaymentController extends Controller
     /**
      * Callback from gateway.
      *
-     * @param  Request $request
+     * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function callback(Request $request)
@@ -30,7 +30,7 @@ class PaymentController extends Controller
         $gateway = resolve(Gateway::class);
         $payment = $this->repo->findByInvoiceId($gateway->getInvoiceIdFromRequest($request));
 
-        if (! $payment) {
+        if (!$payment) {
             ShareService::errorToast('Fail transaction');
             return redirect('/');
         }
@@ -51,41 +51,11 @@ class PaymentController extends Controller
         return redirect()->to($payment->paymentable->path());
     }
 
-    public function index(PaymentRepo $paymentRepo , Request $request)
-    {
-        $this->authorize("manage", Payment::class);
-        $payments = $paymentRepo
-            ->searchEmail($request->email)
-            ->searchAmount($request->amount)
-            ->searchInvoiceId($request->invoice_id)
-            ->searchAfterDate(dateFromJalali($request->start_date))
-            ->searchBeforeDate(dateFromJalali($request->end_date))
-            ->paginate();
-        $last30DaysTotal = $paymentRepo->getLastNDaysTotal(-30);
-        $last30DaysBenefit = $paymentRepo->getLastNDaysSiteBenefit(-30);
-        $last30DaysSellerShare = $paymentRepo->getLastNDaysSellerShare(-30);
-        $totalSell = $paymentRepo->getLastNDaysTotal();
-        $totalBenefit = $paymentRepo->getLastNDaysSiteBenefit();
-        $dates = collect();
-        foreach (range(-30, 0) as $i) {
-            $dates->put(now()->addDays($i)->format("Y-m-d"), 0);
-        }
-        $summery =  $paymentRepo->getDailySummery($dates);
-        return view("Payment::index", compact("payments", "last30DaysTotal", "last30DaysBenefit", "totalSell",
-            "totalBenefit", "last30DaysSellerShare", "summery", "dates"));
-    }
-
-    public function purchases()
-    {
-        $payments = auth()->user()->payments()->with('paymentable')->paginate();
-        return view('Payment::purchases.index' , compact('payments'));
-    }
-
     /**
      * Change payment status.
      *
      * @param  $payment
-     * @param  string $status
+     * @param string $status
      * @return \Illuminate\Http\RedirectResponse
      */
     private function changeStatus($payment, string $status): \Illuminate\Http\RedirectResponse
--- Modules/Payment/Routes/payment_routes.php
+++ Modules/Payment/Routes/payment_routes.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'panel', 'middleware' => 'auth'], static function ($router) {
     $router->any('payments/callback', ['uses' => 'PaymentController@callback', 'as' => 'payment.callback']);
-    $router->get('payments', ['uses' => 'PaymentController@index', 'as' => 'payments.index']);
-    $router->get('purchases', ['uses' => 'PaymentController@purchases', 'as' => 'purchases.index']);
 });
