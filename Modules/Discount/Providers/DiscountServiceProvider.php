f7eae0b0c3ffcda89f8995f19fbb128d37b9fef5
miladev-ent <milwad.dev@gmail.com>
2022-11-19 18:51:29 +0330

    add phpdoc into discount servicer provider

--- Modules/Discount/Providers/DiscountServiceProvider.php
+++ Modules/Discount/Providers/DiscountServiceProvider.php
@@ -7,8 +7,53 @@ use Illuminate\Support\Facades\Route;
 
 class DiscountServiceProvider extends ServiceProvider
 {
-    public string $namespace = 'Modules\Discount\Http\Controllers';
+    /**
+     * Namespace of discount controllers.
+     *
+     * @var string
+     */
+    private string $namespace = 'Modules\Discount\Http\Controllers';
 
+    /**
+     * Namespace of user view files.
+     *
+     * @var string
+     */
+    private string $namespaceDiscountView = 'Discount';
+
+    /**
+     * Path of user migration files.
+     *
+     * @var string
+     */
+    private string $migrationPath = '/../Database/Migrations';
+
+    /**
+     * Path of user view files.
+     *
+     * @var string
+     */
+    private string $viewPath = '/../Resources/Views/';
+
+    /**
+     * Array of middleware routes.
+     *
+     * @var array|string[]
+     */
+    private array $middlewareRoute = ['web', 'verify'];
+
+    /**
+     * Get route path.
+     *
+     * @var string
+     */
+    private string $routePath = '/../Routes/discount_routes.php';
+
+    /**
+     * Register files.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->loadMigrationFiles();
@@ -16,20 +61,35 @@ class DiscountServiceProvider extends ServiceProvider
         $this->loadRouteFiles();
     }
 
+    /**
+     * Load route files.
+     *
+     * @return void
+     */
     private function loadRouteFiles(): void
     {
-        Route::middleware(['web', 'verify'])
+        Route::middleware($this->middlewareRoute)
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../Routes/discount_routes.php');
+            ->group(__DIR__ . $this->routePath);
     }
 
+    /**
+     * Load view files.
+     *
+     * @return void
+     */
     private function loadViewFiles(): void
     {
-        $this->loadViewsFrom(__DIR__ . '/../Resources/Views/', 'Discount');
+        $this->loadViewsFrom(__DIR__ . $this->viewPath, $this->namespaceDiscountView);
     }
 
+    /**
+     * Load migration files.
+     *
+     * @return void
+     */
     private function loadMigrationFiles(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . $this->migrationPath);
     }
 }
