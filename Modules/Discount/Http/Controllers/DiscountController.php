aada615efdf4e08581a798f0a391d4502f68cfee
miladev-ent <milwad.dev@gmail.com>
2022-11-22 21:24:54 +0330

    complete discount controller

--- Modules/Discount/Http/Controllers/DiscountController.php
+++ Modules/Discount/Http/Controllers/DiscountController.php
@@ -3,10 +3,13 @@
 namespace Modules\Discount\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Modules\Discount\Http\Requests\DiscountRequest;
 use Modules\Discount\Models\Discount;
 use Modules\Discount\Repositories\DiscountRepoEloquentInterface;
 use Modules\Discount\Services\DiscountService;
 use Modules\Share\Http\Controllers\Controller;
+use Modules\Share\Responses\AjaxResponses;
+use Modules\Share\Services\ShareService;
 
 class DiscountController extends Controller
 {
@@ -40,54 +43,49 @@ class DiscountController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $this->authorize('manage', $this->class);
+        return view('Discount::index', ['discounts' => $this->repo->getLatest()->paginate()]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        $this->authorize('manage', $this->class);
+        return view('Discount::create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(DiscountRequest $request)
     {
-        //
-    }
+        $this->authorize('manage', $this->class);
+        $this->service->store($request->validated());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \Modules\Discount\Discount  $discount
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Discount $discount)
-    {
-        //
+        return $this->successMessageWithRedirect('Create discount');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \Modules\Discount\Discount  $discount
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function edit(Discount $discount)
     {
-        //
+        $this->authorize('manage', $this->class);
+        return view('Discout::edit', compact('discount'));
     }
 
     /**
@@ -95,21 +93,38 @@ class DiscountController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Modules\Discount\Discount  $discount
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Discount $discount)
+    public function update(DiscountRequest $request, Discount $discount)
     {
-        //
+        $this->authorize('manage', $this->class);
+        $this->service->update($request->validated(), $discount->id);
+
+        return $this->successMessageWithRedirect('Update discount');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \Modules\Discount\Discount  $discount
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Discount $discount)
     {
-        //
+        $discount->delete();
+
+        return AjaxResponses::SuccessResponse();
+    }
+
+    /**
+     * Show success message with redirect;
+     *
+     * @param  string $title
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    private function successMessageWithRedirect(string $title)
+    {
+        ShareService::successToast($title);
+        return to_route('categories.index');
     }
 }
