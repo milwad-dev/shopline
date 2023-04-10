17dbb275710ee0b825eef935906059ab4d879c34
miladev-ent <milwad.dev@gmail.com>
2023-04-10 12:23:57 +0330

    add `factories` key in share config

--- Modules/Share/Config/config.php
+++ Modules/Share/Config/config.php
@@ -5,4 +5,15 @@ return [
     'email'       => 'shopline@gmail.com',
     'address'     => 'Iran, Tehran',
     'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+
+    /*
+     * These factories is used when you run `db:seed` command, for set new factory you can go to related service provider
+     * and set, ex:
+     *
+     * config()->set('shareConfig.factories.{name}', [
+     *     'model'  => User::class,
+     *     'count'  => 1,
+     * ]);
+     */
+    'factories' => []
 ];
