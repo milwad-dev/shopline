581624d0a95e7279bf623992bd13ba10d68d2e59
Milwad <milwad.dev@gmail.com>
2024-10-18 12:31:53 +0330

    fix conflit

--- Modules/Category/Database/Factories/CategoryFactory.php
+++ Modules/Category/Database/Factories/CategoryFactory.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\Category\Enums\CategoryStatusEnum;
 use Modules\Category\Models\Category;
 use Modules\Share\Services\ShareService;
+use Modules\User\Models\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class CategoryFactory extends Factory
 {
     protected $model = Category::class;
@@ -22,7 +20,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => auth()->id(),
+            'user_id' => User::factory()->create()->id,
             'parent_id' => null,
             'title' => $this->faker->title,
             'slug' => ShareService::makeSlug($this->faker->title),
