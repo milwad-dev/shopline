52986009d54ec1aed58019b9fba3af7fb7ceeec2
milwad-dev <98118400+milwad-dev@users.noreply.github.com>
2025-03-20 20:31:58 +0000

    Fixes coding style

--- Modules/RolePermission/Database/Migrations/2022_08_01_140047_create_permission_tables.php
+++ Modules/RolePermission/Database/Migrations/2022_08_01_140047_create_permission_tables.php
@@ -25,7 +25,7 @@ return new class extends Migration
         }
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
-            //$table->engine('InnoDB');
+            // $table->engine('InnoDB');
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
             $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
@@ -35,7 +35,7 @@ return new class extends Migration
         });
 
         Schema::create($tableNames['roles'], function (Blueprint $table) use ($teams, $columnNames) {
-            //$table->engine('InnoDB');
+            // $table->engine('InnoDB');
             $table->bigIncrements('id'); // role id
             if ($teams || config('permission.testing')) { // permission.testing is a fix for sqlite testing
                 $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
